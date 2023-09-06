6cfaa18e55deea38a57a1de78de6e5fec88e98ea
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2023-09-06 07:53:34 -0400

    Update BTSLogPoseClient.java

--- src/main/java/tfar/btslogpose/client/BTSLogPoseClient.java
+++ src/main/java/tfar/btslogpose/client/BTSLogPoseClient.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -28,40 +29,44 @@ public class BTSLogPoseClient {
     @SubscribeEvent
     public static void renderPings(RenderWorldLastEvent e) {
         for (BTSPing ping : pings) {
-            Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation(BTSLogPose.MOD_ID,"textures/gui/ping_blue.png"));
             renderTooltip3D(Minecraft.getMinecraft(),ping,e.getPartialTicks());
             Minecraft.getMinecraft().getTextureManager().bindTexture(Gui.ICONS);
         }
     }
 
     public static void renderTooltip3D(Minecraft mc, BTSPing ping,double partialTicks) {
-        ScaledResolution sr = new ScaledResolution(mc);
         BlockPos pos = ping.getPos();
-        int alpha = 0xff;
         int outline1 = ping.getColor();
+        Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation(BTSLogPose.MOD_ID,"textures/gui/ping_blue.png"));
         double xpos = mc.getRenderManager().viewerPosX - pos.getX();
         double ypos = mc.getRenderManager().viewerPosY - pos.getY();
         double zpos = mc.getRenderManager().viewerPosZ - pos.getZ();
-        double dist = Math.sqrt(xpos * xpos + ypos * ypos + zpos * zpos);
-        double scale = 1/40f * (1 + dist/100);
-        //scale /= sr.getScaleFactor() * 16;
-     //   if (scale <= 0.01)
-     //       scale = 0.01;
-        //RenderHelper.start();
+
+        final double actualDistance = Minecraft.getMinecraft().player.getDistance(pos.getX(),pos.getY(),pos.getZ());
+
+        double viewDistance = actualDistance;
+        final double maxRenderDistance = mc.gameSettings.renderDistanceChunks * 16;
+        if (viewDistance > maxRenderDistance) {
+            final Vec3d delta =new Vec3d(xpos,ypos,zpos).normalize();
+            xpos = delta.x * maxRenderDistance;
+            ypos = delta.y * maxRenderDistance;
+            zpos = delta.z * maxRenderDistance;
+            viewDistance = maxRenderDistance;
+        }
+        final double maxScale = 1/20f;
+        double scaleMultiplier = Math.max(1,actualDistance / maxRenderDistance);
+        double scale = maxScale / scaleMultiplier;
+
+        double effectiveScale = maxScale / (Math.min(4,Math.sqrt(scaleMultiplier)));
+
+        GlStateManager.pushMatrix();
         GlStateManager.translate(-xpos, -ypos, -zpos);
         GlStateManager.rotate(-mc.getRenderManager().playerViewY + 180, 0, 1, 0);
         GlStateManager.rotate(-mc.getRenderManager().playerViewX, 1, 0, 0);
-        GlStateManager.scale(scale, -scale, scale);
+        GlStateManager.scale(effectiveScale, -effectiveScale, effectiveScale);
         GlStateManager.disableDepth();
-
         Gui.drawModalRectWithCustomSizedTexture(0,0,0,0,128,128,128,128);
-
-//        GlStateManager.enableDepth();
-        GlStateManager.scale(1F / scale, 1F / -scale, 1F / scale);
-        GlStateManager.rotate(mc.getRenderManager().playerViewX, 1, 0, 0);
-        GlStateManager.rotate(mc.getRenderManager().playerViewY - 180, 0, 1, 0);
-        GlStateManager.translate(xpos, ypos, zpos);
-        //RenderHelper.end();
+        GlStateManager.enableDepth();
+        GlStateManager.popMatrix();
     }
-
 }
